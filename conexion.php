093cda45251748d45b685008051a9dc76b57db88
Yordi <yordi.cruz@gmail.com>
2025-04-12 17:32:34 -0400

    Eliminar conexion.php del repositorio

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost";
-$usudb="root";
-$password="";
-$bd="sistema_almacen";
-$conec = mysqli_connect("$host", "$usudb", "$password", "$bd");
-// ruta absoluta para hacer busquedas
-$base_url ="http://localhost/php,js,html/";
-?>
